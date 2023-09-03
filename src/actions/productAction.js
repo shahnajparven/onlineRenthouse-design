41bde4fe12fe2590ec10c2335972a7164d2048bd
shahnaj parven <shahnajritu21@gmail.com>
2023-09-03 21:02:38 +0600

    changes

--- src/actions/productAction.js
+++ src/actions/productAction.js
@@ -33,10 +33,10 @@ export const getProduct = (keyword = "", currentPage = 1, price = [0, 25000], ca
     dispatch({ type: ALL_PRODUCT_REQUEST });
 
 
-    let link =`/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
+    let link =`https://renthouse-server.onrender.com/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
 
     if(category) {
-      link =`/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
+      link =`https://renthouse-server.onrender.com/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
     }
 
     // const { data } = await axios.get(link);
